4e2cd873079a205b1b0c75af1ccb0359d14b0169
Jonathan Goclowski <JRGoclowski@gmail.com>
2020-10-30 16:57:51 -0700

    Now create lists of ints from integers

--- src/main/java/cecs429/index/ByteUtils.java
+++ src/main/java/cecs429/index/ByteUtils.java
@@ -8,6 +8,7 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class ByteUtils {
     private static ByteArrayOutputStream byteArrayStream = new ByteArrayOutputStream();
     private static DataOutputStream byteOutStream = new DataOutputStream(byteArrayStream);
@@ -26,18 +27,21 @@ public class ByteUtils {
         //System.out.println("Byte Array (Hex) : " + convertBytesToHex(result));
         */
 
-        List<List<Byte>> encodeResults = new ArrayList<List<Byte>>();
+        List<List<List<Integer>>> encodeResults = new ArrayList<List<List<Integer>>>();
 
-        encodeResults.add(variableByteEncode(7));
-        encodeResults.add(variableByteEncode(130));
-        encodeResults.add(variableByteEncode(240));
-        encodeResults.add(variableByteEncode(470));
-        encodeResults.add(variableByteEncode(60000));
+        //encodeResults.add(variableByteEncode(7));
+        encodeResults.add(createByteList(173));
+        encodeResults.add(createByteList(203));
+        encodeResults.add(createByteList(252));
 
-        for (List<Byte> lbyte : encodeResults) {
-            System.out.println("Byte: " + lbyte.toString());
-            System.out.println("Integer: " + variableByteDecode(lbyte));
-        }
+        encodeResults.add(createByteList(130));
+        encodeResults.add(createByteList(240));
+        encodeResults.add(createByteList(470));
+        encodeResults.add(createByteList(60000));        
+        encodeResults.add(createByteList(2460000));
+        encodeResults.add(createByteList(245900000));
+        encodeResults.add(createByteList(2147483647));
+
     }
     
     //-------------------------------------------------------------------------
@@ -50,12 +54,14 @@ public class ByteUtils {
         return ByteBuffer.wrap(bytes).getInt();
     }
 
-    public static List<Integer> convertToBinary(Integer n) {
-        int[] binary = new int[40];
+    public static List<Integer> convertIntToBinList(Integer n) {
+        int[] binary = new int[8];
         int index = 0;
-        while (n > 0) {
-            binary[index++] = n % 2;
-            n = n / 2;
+        Integer convertInt = Byte.toUnsignedInt(n.byteValue());        
+        for (int i = 0; i < 8; i++) {
+            int testVal = convertInt % 2;
+            binary[index++] = testVal;
+            convertInt = convertInt / 2;
         }
 
         List<Integer> realBinary = new ArrayList<>();
@@ -66,16 +72,19 @@ public class ByteUtils {
     }
 
 
-    public static List<Byte> variableByteEncode(Integer n) {
+    public static List<List<Integer>> createByteList(Integer n) {
 
-        byte[] result = getByteArray(n);
-        for (Byte lbyte : result) {
+        byte[] intBytes = getByteArray(n); 
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+        for (Byte lbyte : intBytes) {
             int byteToInt = lbyte.intValue();
-            List<Integer> temp = convertToBinary(byteToInt);
-            System.out.print(temp.toString() + " ");
+            List<Integer> temp = convertIntToBinList(byteToInt);
+            results.add(temp);
+            //System.out.print(temp.toString() + " ");
             //System.out.println(convertToBinary(lbyte));
         }
-        return null;
+        //System.out.println();
+        return results;
     }
 
     public static Integer variableByteDecode(List<Byte> byteList) {
@@ -136,16 +145,16 @@ public class ByteUtils {
     }
 
     public static byte[] getByteArray(Integer integerArg) {
-        byte[] resultArray = null;
-        try {            
-            byteOutStream.writeInt(integerArg);
-            byteOutStream.flush();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        resultArray = byteArrayStream.toByteArray();
-        byteArrayStream.reset();        
+        byte[] resultArray = ByteBuffer.allocate(4).putInt(integerArg).array();
+        // try {            
+        //     byteOutStream.writeInt(integerArg);
+        //     byteOutStream.flush();
+        // } catch (IOException e) {
+        //     // TODO Auto-generated catch block
+        //     e.printStackTrace();
+        // }
+        // resultArray = byteArrayStream.toByteArray();
+        // byteArrayStream.reset();        
         return resultArray;
     }
 
