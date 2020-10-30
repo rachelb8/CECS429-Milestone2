45fa0cf5961e8501a36826132edb9cf800df9963
danielmendez7899 <44877100+danielmendez7899@users.noreply.github.com>
2020-10-30 12:09:01 -0700

    Work in Progress

--- src/main/java/cecs429/index/ByteUtils.java
+++ src/main/java/cecs429/index/ByteUtils.java
@@ -4,12 +4,88 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ByteUtils {
     private static ByteArrayOutputStream byteArrayStream = new ByteArrayOutputStream();
     private static DataOutputStream byteOutStream = new DataOutputStream(byteArrayStream);
+
+    public static void main(String[] args) {
+
+        /*
+        int num = 470;
+        byte[] result = convertIntToByteArray(num);
+
+        System.out.println("Input            : " + num);
+        for (Byte lbyte : result) {
+            String resStr = String.format("%8s", Integer.toBinaryString(lbyte & 0xFF)).replace(' ', '0');
+            System.out.print(resStr + " ");
+        }
+        //System.out.println("Byte Array (Hex) : " + convertBytesToHex(result));
+        */
+
+        List<List<Byte>> encodeResults = new ArrayList<List<Byte>>();
+
+        encodeResults.add(variableByteEncode(7));
+        encodeResults.add(variableByteEncode(130));
+        encodeResults.add(variableByteEncode(240));
+        encodeResults.add(variableByteEncode(470));
+        encodeResults.add(variableByteEncode(60000));
+
+        for (List<Byte> lbyte : encodeResults) {
+            System.out.println("Byte: " + lbyte.toString());
+            System.out.println("Integer: " + variableByteDecode(lbyte));
+        }
+    }
+    
+    //-------------------------------------------------------------------------
+
+    public static byte[] convertIntToByteArray(int value) {
+        return  ByteBuffer.allocate(4).putInt(value).array();
+    }
+
+     public static int convertByteArrayToBinaryArray(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).getInt();
+    }
+
+    public static List<Integer> convertToBinary(Integer n) {
+        int[] binary = new int[40];
+        int index = 0;
+        while (n > 0) {
+            binary[index++] = n % 2;
+            n = n / 2;
+        }
+
+        List<Integer> realBinary = new ArrayList<>();
+        for (int i = index-1; i >= 0; i--) {
+            realBinary.add(binary[i]);
+        }
+        return realBinary;
+    }
+
+
+    public static List<Byte> variableByteEncode(Integer n) {
+
+        byte[] result = getByteArray(n);
+        for (Byte lbyte : result) {
+            int byteToInt = lbyte.intValue();
+            List<Integer> temp = convertToBinary(byteToInt);
+            System.out.print(temp.toString() + " ");
+            //System.out.println(convertToBinary(lbyte));
+        }
+        return null;
+    }
+
+    public static Integer variableByteDecode(List<Byte> byteList) {
+        Integer result = 0;
+        return result;
+    }
+
+   
+
+    //-------------------------------------------------------------------------
 
     public static Integer DecodeNextInt(DataInputStream dataInputStreamArg){
         Integer result;
@@ -86,6 +162,9 @@ public class ByteUtils {
         byteArrayStream.reset();  
         return resultArray;
     }
+
+   
+
 
     public static void appendToArrayList(List<Byte> arrayArg, byte[] byteArg){
         for (byte lByte : byteArg){
